9b74bf2430260f8239efb957140e68fe8b0ebe0a
Wahyu Studiawan <manusiasampah7@gmail.com>
2022-02-28 00:47:50 +0700

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -192,7 +192,7 @@ DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
 	if [ $COMPILER = "clang" ]
 	then
 		msg "|| Cloning Clang-14 ||"
-		git clone --depth=1 https://gitlab.com/Panchajanya1999/azure-clang.git clang-llvm
+		git clone --depth=1 https://github.com/Wstudiawan/AOSP-Clang.git clang-llvm
 		# Toolchain Directory defaults to clang-llvm
 		TC_DIR=$KERNEL_DIR/clang-llvm
 	fi
